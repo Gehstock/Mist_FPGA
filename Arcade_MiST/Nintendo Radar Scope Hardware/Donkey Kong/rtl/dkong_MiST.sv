c0687b62612af6794faf7dd549a4e9d2ea900482
Gyorgy Szombathelyi <gyurco@freemail.hu>
2019-10-27 14:20:10 +0100

    Update Donkey Kong - seems to work now

--- Arcade_MiST/Nintendo Radar Scope Hardware/Donkey Kong/rtl/dkong_MiST.sv
+++ Arcade_MiST/Nintendo Radar Scope Hardware/Donkey Kong/rtl/dkong_MiST.sv
@@ -23,6 +23,7 @@ localparam CONF_STR = {
 	"DKONG;;",
 	"O2,Rotate Controls,Off,On;",
 	"O34,Scanlines,Off,25%,50%,75%;",
+	"O5,Blending,Off,On;",
 	"T6,Reset;",
 	"O89,Lives,3,4,5,6;",
 	"OAB,Bonus,7000,10000,15000,20000;",
@@ -36,7 +37,6 @@ wire clock_24, clock_6;
 pll pll(
 	.inclk0(CLOCK_27),
 	.c0(clock_24),//W_CLK_24576M
-	.c2(clock_6)//W_CLK_6144M
 	);
 
 wire [31:0] status;
@@ -47,19 +47,16 @@ wire  [7:0] joystick_0;
 wire  [7:0] joystick_1;
 wire        scandoublerD;
 wire        ypbpr;
-wire [10:0] ps2_key;
-wire [7:0] audio;
-wire 			hs, vs;
-wire 			hb, vb;
-wire 			blankn = ~(hb | vb);
+wire  [7:0] audio;
+wire        hs_n, vs_n;
+wire        hb, vb;
+wire        blankn = ~vb;//~(hb | vb);
 wire [2:0] 	r, g;
 wire [1:0] 	b;
-
 	
 dkong_top dkong(				   
 	.I_CLK_24576M(clock_24),
 	.I_RESETn(~(status[0] | status[6] | buttons[1])),
-//	.O_PIX(clock_6),//Timing Issue
 	.I_U1(~m_up),
 	.I_D1(~m_down),
 	.I_L1(~m_left),
@@ -80,70 +77,68 @@ dkong_top dkong(
 	.O_VGA_B(b),
 	.O_H_BLANK(hb),
 	.O_V_BLANK(vb),
-	.O_VGA_H_SYNCn(hs),
-	.O_VGA_V_SYNCn(vs)
+	.O_VGA_H_SYNCn(hs_n),
+	.O_VGA_V_SYNCn(vs_n)
 	);
 
-
-video_mixer video_mixer(
+mist_video #(.COLOR_DEPTH(3),.SD_HCNT_WIDTH(10)) mist_video(
 	.clk_sys(clock_24),
-	.ce_pix(clock_6),
-	.ce_pix_actual(clock_6),
 	.SPI_SCK(SPI_SCK),
 	.SPI_SS3(SPI_SS3),
 	.SPI_DI(SPI_DI),
-	.R(blankn ? r : "000"),
-	.G(blankn ? g : "000"),
-	.B(blankn ? {1'b0,b} : "000"),
-	.HSync(hs),
-	.VSync(vs),
+	.R(blankn ? r : 0),
+	.G(blankn ? g : 0),
+	.B(blankn ? {b[1], b} : 0),
+	.HSync(hs_n),
+	.VSync(vs_n),
 	.VGA_R(VGA_R),
 	.VGA_G(VGA_G),
 	.VGA_B(VGA_B),
 	.VGA_VS(VGA_VS),
 	.VGA_HS(VGA_HS),
 	.rotate({1'b1,status[2]}),
-	.scandoublerD(scandoublerD),
-	.scanlines(scandoublerD ? 2'b00 : status[4:3]),
-	.ypbpr(ypbpr),
-	.ypbpr_full(1),
-	.line_start(0),
-	.mono(0)
+	.ce_divider(1'b1),
+	.blend(status[5]),
+	.scandoubler_disable(scandoublerD),
+	.scanlines(status[4:3]),
+	.ypbpr(ypbpr)
 	);
 
-mist_io #(
+user_io #(
 	.STRLEN(($size(CONF_STR)>>3)))
-mist_io(
+user_io(
 	.clk_sys        (clock_24       ),
 	.conf_str       (CONF_STR       ),
-	.SPI_SCK        (SPI_SCK        ),
-	.CONF_DATA0     (CONF_DATA0     ),
-	.SPI_SS2			 (SPI_SS2        ),
-	.SPI_DO         (SPI_DO         ),
-	.SPI_DI         (SPI_DI         ),
+	.SPI_CLK        (SPI_SCK        ),
+	.SPI_SS_IO      (CONF_DATA0     ),
+	.SPI_MISO       (SPI_DO         ),
+	.SPI_MOSI       (SPI_DI         ),
 	.buttons        (buttons        ),
-	.switches   	 (switches       ),
-	.scandoublerD	 (scandoublerD	  ),
+	.switches       (switches       ),
+	.scandoubler_disable (scandoublerD	  ),
 	.ypbpr          (ypbpr          ),
-	.ps2_key			 (ps2_key        ),
-	.joystick_0   	 (joystick_0     ),
+	.key_strobe     (key_strobe     ),
+	.key_pressed    (key_pressed    ),
+	.key_code       (key_code       ),
+	.joystick_0     (joystick_0     ),
 	.joystick_1     (joystick_1     ),
 	.status         (status         )
 	);
-	
+
 dac #(
-	.C_bits(15))
+	.C_bits(8))
 dac(
 	.clk_i(clock_24),
 	.res_n_i(1'b1),
-	.dac_i({audio,audio}),
+	.dac_i(audio),
 	.dac_o(AUDIO_L)
 	);
-	
-wire m_up     = btn_up | joystick_0[3] | joystick_1[3];
-wire m_down   = btn_down | joystick_0[2] | joystick_1[2];
-wire m_left   = btn_left | joystick_0[1] | joystick_1[1];
-wire m_right  = btn_right | joystick_0[0] | joystick_1[0];
+
+//											Rotated														Normal
+wire m_up     = ~status[2] ? btn_left | joystick_0[1] | joystick_1[1] : btn_up | joystick_0[3] | joystick_1[3];
+wire m_down   = ~status[2] ? btn_right | joystick_0[0] | joystick_1[0] : btn_down | joystick_0[2] | joystick_1[2];
+wire m_left   = ~status[2] ? btn_down | joystick_0[2] | joystick_1[2] : btn_left | joystick_0[1] | joystick_1[1];
+wire m_right  = ~status[2] ? btn_up | joystick_0[3] | joystick_1[3] : btn_right | joystick_0[0] | joystick_1[0];
 wire m_fire   = btn_fire1 | joystick_0[4] | joystick_1[4];
 
 reg btn_one_player = 0;
@@ -154,22 +149,22 @@ reg btn_down = 0;
 reg btn_up = 0;
 reg btn_fire1 = 0;
 reg btn_coin  = 0;
-wire       pressed = ps2_key[9];
-wire [7:0] code    = ps2_key[7:0];	
+wire       key_pressed;
+wire [7:0] key_code;
+wire       key_strobe;
 
 always @(posedge clock_24) begin
 	reg old_state;
-	old_state <= ps2_key[10];
-	if(old_state != ps2_key[10]) begin
-		case(code)
-			'h75: btn_up         	<= pressed; // up
-			'h72: btn_down        	<= pressed; // down
-			'h6B: btn_left      		<= pressed; // left
-			'h74: btn_right       	<= pressed; // right
-			'h76: btn_coin				<= pressed; // ESC
-			'h05: btn_one_player   	<= pressed; // F1
-			'h06: btn_two_players  	<= pressed; // F2
-			'h29: btn_fire1   		<= pressed; // Space
+	if(key_strobe) begin
+		case(key_code)
+			'h75: btn_up         	<= key_pressed; // up
+			'h72: btn_down        	<= key_pressed; // down
+			'h6B: btn_left      	<= key_pressed; // left
+			'h74: btn_right       	<= key_pressed; // right
+			'h76: btn_coin			<= key_pressed; // ESC
+			'h05: btn_one_player   	<= key_pressed; // F1
+			'h06: btn_two_players  	<= key_pressed; // F2
+			'h29: btn_fire1   		<= key_pressed; // Space
 		endcase
 	end
 end
