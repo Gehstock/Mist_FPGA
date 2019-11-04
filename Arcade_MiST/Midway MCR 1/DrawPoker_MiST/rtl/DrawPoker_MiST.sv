c289cef7881cf289ceb5fd75c8fbf2c30218bd71
Gehstock <marceljenke@gmail.com>
2019-11-04 20:10:46 +0100

    Add Draw Poker Code - WIP

---
+++ Arcade_MiST/Midway MCR 1/DrawPoker_MiST/rtl/DrawPoker_MiST.sv
@@ -0,0 +1,251 @@
+//============================================================================
+//  Arcade: Draw Poker by DarFPGA
+//
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License as published by the Free
+//  Software Foundation; either version 2 of the License, or (at your option)
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//============================================================================
+
+module DrawPoker_MiST(
+	output        LED,						
+	output  [5:0] VGA_R,
+	output  [5:0] VGA_G,
+	output  [5:0] VGA_B,
+	output        VGA_HS,
+	output        VGA_VS,
+	output        AUDIO_L,
+	output        AUDIO_R,	
+	input         SPI_SCK,
+	output        SPI_DO,
+	input         SPI_DI,
+	input         SPI_SS2,
+	input         SPI_SS3,
+	input         CONF_DATA0,
+	input         CLOCK_27,
+
+	output [12:0] SDRAM_A,
+	inout  [15:0] SDRAM_DQ,
+	output        SDRAM_DQML,
+	output        SDRAM_DQMH,
+	output        SDRAM_nWE,
+	output        SDRAM_nCAS,
+	output        SDRAM_nRAS,
+	output        SDRAM_nCS,
+	output  [1:0] SDRAM_BA,
+	output        SDRAM_CLK,
+	output        SDRAM_CKE
+);
+
+`include "rtl/build_id.v" 
+
+localparam CONF_STR = {
+	"DPOKER;;",
+	"O34,Scanlines,Off,25%,50%,75%;",
+	"O5,Blend,Off,On;",//ugly, Frequency?
+	"O6,BG Color ,Green,Blue;",
+	"T0,Reset;",
+	"V,v1.0.",`BUILD_DATE
+};
+
+assign LED = ~ioctl_downl;
+assign SDRAM_CLK = clk_sys;
+
+wire clk_sys;
+wire pll_locked;
+pll_mist pll(
+	.inclk0(CLOCK_27),
+	.areset(0),
+	.c0(clk_sys),
+	.locked(pll_locked)
+	);
+
+wire [31:0] status;
+wire  [1:0] buttons;
+wire  [1:0] switches;
+wire  [7:0] joystick_0;
+wire  [7:0] joystick_1;
+wire        scandoublerD;
+wire        ypbpr;
+wire [15:0] audio_l, audio_r;
+wire        hs, vs;
+wire        blankn;
+wire  [3:0] g, r, b;
+wire [14:0] rom_addr;
+wire [15:0] rom_do;
+wire        rom_rd;
+wire        ioctl_downl;
+wire  [7:0] ioctl_index;
+wire        ioctl_wr;
+wire [24:0] ioctl_addr;
+wire  [7:0] ioctl_dout;
+
+data_io data_io(
+	.clk_sys       ( clk_sys      ),
+	.SPI_SCK       ( SPI_SCK      ),
+	.SPI_SS2       ( SPI_SS2      ),
+	.SPI_DI        ( SPI_DI       ),
+	.ioctl_download( ioctl_downl  ),
+	.ioctl_index   ( ioctl_index  ),
+	.ioctl_wr      ( ioctl_wr     ),
+	.ioctl_addr    ( ioctl_addr   ),
+	.ioctl_dout    ( ioctl_dout   )
+);
+		
+sdram cart(
+	.*,
+	.init          ( ~pll_locked  ),
+	.clk           ( clk_sys      ),
+	.wtbt          ( 2'b00        ),
+	.dout          ( rom_do     ),
+	.din           ( {ioctl_dout, ioctl_dout} ),
+	.addr          ( ioctl_downl ? ioctl_addr : rom_addr ),
+	.we            ( ioctl_downl & ioctl_wr ),
+	.rd            ( !ioctl_downl ),
+	.ready()
+);
+
+reg reset = 1;
+reg rom_loaded = 0;
+always @(posedge clk_sys) begin
+	reg ioctl_downlD;
+	ioctl_downlD <= ioctl_downl;
+
+	if (ioctl_downlD & ~ioctl_downl) rom_loaded <= 1;
+	reset <= status[0] | buttons[1] | ~rom_loaded;
+end
+
+kick kick(
+	.clock_40(clk_sys),
+	.reset(reset),
+	.video_r(r),
+	.video_g(g),
+	.video_b(b),
+	.video_blankn(blankn),
+	.video_hs(hs),
+	.video_vs(vs),
+	.separate_audio(1'b0),
+	.audio_out_l(audio_l),
+	.audio_out_r(audio_r),
+	.bgcolor(status[6]),
+	.STAND(btn_stand), 
+	.CANCEL(btn_cancel), 
+	.DEAL(btn_deal), 
+	.HOLD5(btn_hold5), 
+	.HOLD4(btn_hold4), 
+	.HOLD3(btn_hold3), 
+	.HOLD2(btn_hold2),
+	.HOLD1(btn_hold1),
+	.COIN1(btn_coin),
+	.COIN2(1'b0),
+	.GAMBLE_IN(btn_gamble_in),
+	.GAMBLE_OUT(btn_gamble_out),
+	.cpu_rom_addr ( rom_addr        ),
+	.cpu_rom_do   ( rom_do[7:0]     ),
+	.cpu_rom_rd   ( rom_rd          )
+ );
+
+mist_video #(.COLOR_DEPTH(4), .SD_HCNT_WIDTH(10)) mist_video(
+	.clk_sys        ( clk_sys          ),
+	.SPI_SCK        ( SPI_SCK          ),
+	.SPI_SS3        ( SPI_SS3          ),
+	.SPI_DI         ( SPI_DI           ),
+	.R              ( blankn ? r : 0   ),
+	.G              ( blankn ? g : 0   ),
+	.B              ( blankn ? b : 0   ),
+	.HSync          ( hs               ),
+	.VSync          ( vs               ),
+	.VGA_R          ( VGA_R            ),
+	.VGA_G          ( VGA_G            ),
+	.VGA_B          ( VGA_B            ),
+	.VGA_VS         ( VGA_VS           ),
+	.VGA_HS         ( VGA_HS           ),
+	.blend(status[5]),
+	.scandoubler_disable(1),//scandoublerD ),
+	.scanlines      ( status[4:3]      ),
+	.ypbpr          ( ypbpr            )
+	);
+
+user_io #(
+	.STRLEN(($size(CONF_STR)>>3)))
+user_io(
+	.clk_sys        (clk_sys        ),
+	.conf_str       (CONF_STR       ),
+	.SPI_CLK        (SPI_SCK        ),
+	.SPI_SS_IO      (CONF_DATA0     ),
+	.SPI_MISO       (SPI_DO         ),
+	.SPI_MOSI       (SPI_DI         ),
+	.buttons        (buttons        ),
+	.switches       (switches       ),
+	.scandoubler_disable (scandoublerD	  ),
+	.ypbpr          (ypbpr          ),
+	.key_strobe     (key_strobe     ),
+	.key_pressed    (key_pressed    ),
+	.key_code       (key_code       ),
+	.joystick_0     (joystick_0     ),
+	.joystick_1     (joystick_1     ),
+	.status         (status         )
+	);
+
+dac #(
+	.C_bits(16))
+dac_l(
+	.clk_i(clk_sys),
+	.res_n_i(1),
+	.dac_i(audio_l),
+	.dac_o(AUDIO_L)
+	);
+	
+dac #(
+	.C_bits(16))
+dac_r(
+	.clk_i(clk_sys),
+	.res_n_i(1),
+	.dac_i(audio_r),
+	.dac_o(AUDIO_R)
+	);	
+
+	
+reg btn_gamble_in = 0;
+reg btn_gamble_out = 0;
+reg btn_cancel = 0;
+reg btn_deal = 0;
+reg btn_hold1 = 0;
+reg btn_hold2 = 0;
+reg btn_hold3 = 0;
+reg btn_hold4 = 0;
+reg btn_hold5 = 0;
+reg btn_coin  = 0;
+wire       key_pressed;
+wire [7:0] key_code;
+wire       key_strobe;
+
+always @(posedge clk_sys) begin
+	if(key_strobe) begin
+		case(key_code)			
+			'h76: btn_coin        <= key_pressed; // ESC
+			'h11: btn_stand       <= key_pressed; // left ALT			
+			'h14: btn_cancel      <= key_pressed; // left ctrl
+			'h29: btn_deal        <= key_pressed; // Space
+			'h2E: btn_hold5       <= key_pressed; // 5
+			'h25: btn_hold4       <= key_pressed; // 4
+			'h26: btn_hold3       <= key_pressed; // 3
+			'h1E: btn_hold2       <= key_pressed; // 2
+			'h16: btn_hold1       <= key_pressed; // 1
+			
+			'h75: btn_gamble_in       <= key_pressed; // Up
+			'h72: btn_gamble_out      <= key_pressed; // Down
+		endcase
+	end
+end
+
+endmodule 
