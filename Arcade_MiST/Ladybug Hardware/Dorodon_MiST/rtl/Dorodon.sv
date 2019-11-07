3351ba844f3a204c9dcf245ea0962ef8a0e59a16
Gyorgy Szombathelyi <gyurco@freemail.hu>
2019-11-07 23:33:08 +0100

    Ladybug: update to common MiST components

--- Arcade_MiST/Ladybug Hardware/Dorodon_MiST/rtl/Dorodon.sv
+++ Arcade_MiST/Ladybug Hardware/Dorodon_MiST/rtl/Dorodon.sv
@@ -44,6 +44,7 @@ localparam CONF_STR = {
 	"Dorodon;;",
 	"O2,Rotate Controls,Off,On;",
 	"O34,Scanlines,Off,25%,50%,75%;",
+	"O5,Blend,Off,On;",
 	"T6,Reset;",
 	"V,v1.10.",`BUILD_DATE
 };
@@ -97,17 +98,15 @@ ladybugt dorodon(
 	.but_left_s(~{1'b0,m_left}),
 	.but_right_s(~{1'b0,m_right})
 	);
-	
-video_mixer video_mixer(
+
+mist_video #(.COLOR_DEPTH(2),.SD_HCNT_WIDTH(10)) mist_video(
 	.clk_sys(clk_sys),
-	.ce_pix(ce_vid),
-	.ce_pix_actual(ce_vid),
 	.SPI_SCK(SPI_SCK),
 	.SPI_SS3(SPI_SS3),
 	.SPI_DI(SPI_DI),
-	.R(blankn ? {r,r[1]} : "000"),
-	.G(blankn ? {g,g[1]} : "000"),
-	.B(blankn ? {b,b[1]} : "000"),
+	.R(blankn ? r : 0),
+	.G(blankn ? g : 0),
+	.B(blankn ? b : 0),
 	.HSync(hs),
 	.VSync(vs),
 	.VGA_R(VGA_R),
@@ -116,41 +115,41 @@ video_mixer video_mixer(
 	.VGA_VS(VGA_VS),
 	.VGA_HS(VGA_HS),
 	.rotate({1'b0,status[2]}),
-	.scandoublerD(scandoublerD),
-	.scanlines(scandoublerD ? 2'b00 : status[4:3]),
-	.ypbpr(ypbpr),
-	.ypbpr_full(1),
-	.line_start(0),
-	.mono(0)
-	);
+	.ce_divider(1'b1),
+	.blend(status[5]),
+	.scandoubler_disable(scandoublerD),
+	.scanlines(status[4:3]),
+	.ypbpr(ypbpr)
+);
 
-mist_io #(
+user_io #(
 	.STRLEN(($size(CONF_STR)>>3)))
-mist_io(
+user_io(
 	.clk_sys        (clk_sys        ),
 	.conf_str       (CONF_STR       ),
-	.SPI_SCK        (SPI_SCK        ),
-	.CONF_DATA0     (CONF_DATA0     ),
-	.SPI_SS2			 (SPI_SS2        ),
-	.SPI_DO         (SPI_DO         ),
-	.SPI_DI         (SPI_DI         ),
+	.SPI_CLK        (SPI_SCK        ),
+	.SPI_SS_IO      (CONF_DATA0     ),
+	.SPI_MISO       (SPI_DO         ),
+	.SPI_MOSI       (SPI_DI         ),
 	.buttons        (buttons        ),
-	.switches   	 (switches       ),
-	.scandoublerD	 (scandoublerD	  ),
+	.switches       (switches       ),
+	.scandoubler_disable (scandoublerD),
 	.ypbpr          (ypbpr          ),
-	.ps2_key			 (ps2_key        ),
-	.joystick_0   	 (joystick_0     ),
+	.key_strobe     (key_strobe     ),
+	.key_pressed    (key_pressed    ),
+	.key_code       (key_code       ),
+	.joystick_0     (joystick_0     ),
 	.joystick_1     (joystick_1     ),
 	.status         (status         )
-	);
+);
 
 dac dac(
 	.clk_i(clk_sys),
 	.res_n_i(1),
-	.dac_i({~audio[7], audio[6:0], 8'b00000000}),
+	.dac_i({~audio[7], audio[6:0]}),
 	.dac_o(AUDIO_L)
 	);
-	
+
 wire m_up     = ~status[2] ? btn_right | joystick_0[0] | joystick_1[0] : btn_up | joystick_0[3] | joystick_1[3];
 wire m_down   = ~status[2] ? btn_left | joystick_0[1] | joystick_1[1] : btn_down | joystick_0[2] | joystick_1[2];
 wire m_left   = ~status[2] ? btn_up | joystick_0[3] | joystick_1[3] : btn_left | joystick_0[1] | joystick_1[1];
@@ -169,24 +168,23 @@ reg btn_fire1 = 0;
 reg btn_fire2 = 0;
 reg btn_fire3 = 0;
 reg btn_coin  = 0;
-wire       pressed = ps2_key[9];
-wire [7:0] code    = ps2_key[7:0];	
+wire       key_pressed;
+wire [7:0] key_code;
+wire       key_strobe;
 
 always @(posedge clk_sys) begin
-	reg old_state;
-	old_state <= ps2_key[10];
-	if(old_state != ps2_key[10]) begin
-		case(code)
-			'h75: btn_up         	<= pressed; // up
-			'h72: btn_down        	<= pressed; // down
-			'h6B: btn_left      		<= pressed; // left
-			'h74: btn_right       	<= pressed; // right
-			'h76: btn_coin				<= pressed; // ESC
-			'h05: btn_one_player   	<= pressed; // F1
-			'h06: btn_two_players  	<= pressed; // F2
-			'h14: btn_fire3 			<= pressed; // ctrl
-			'h11: btn_fire2 			<= pressed; // alt
-			'h29: btn_fire1   		<= pressed; // Space
+	if(key_strobe) begin
+		case(key_code)
+			'h75: btn_up         	<= key_pressed; // up
+			'h72: btn_down        	<= key_pressed; // down
+			'h6B: btn_left      		<= key_pressed; // left
+			'h74: btn_right       	<= key_pressed; // right
+			'h76: btn_coin				<= key_pressed; // ESC
+			'h05: btn_one_player   	<= key_pressed; // F1
+			'h06: btn_two_players  	<= key_pressed; // F2
+			'h14: btn_fire3 			<= key_pressed; // ctrl
+			'h11: btn_fire2 			<= key_pressed; // alt
+			'h29: btn_fire1   		<= key_pressed; // Space
 		endcase
 	end
 end
