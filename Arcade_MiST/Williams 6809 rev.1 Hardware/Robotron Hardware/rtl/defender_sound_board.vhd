877750e4481b9b311db3bfb89d5fe9eb558bdcb2
Gyorgy Szombathelyi <gyurco@freemail.hu>
2020-03-11 18:59:25 +0100

    Robotron HW: add speech chip for Sinistar

--- Arcade_MiST/Williams 6809 rev.1 Hardware/Robotron Hardware/rtl/defender_sound_board.vhd
+++ Arcade_MiST/Williams 6809 rev.1 Hardware/Robotron Hardware/rtl/defender_sound_board.vhd
@@ -32,8 +32,10 @@ port(
  hand         : out std_logic;
  select_sound : in  std_logic_vector( 5 downto 0);
  audio_out    : out std_logic_vector( 7 downto 0);
- rom_addr     : out std_logic_vector(11 downto 0);
+ speech_out   : out std_logic_vector(15 downto 0);
+ rom_addr     : out std_logic_vector(13 downto 0);
  rom_do       : in  std_logic_vector( 7 downto 0);
+ spch_do      : in  std_logic_vector( 7 downto 0);
  rom_vma      : out std_logic
 );
 end defender_sound_board;
@@ -54,6 +56,8 @@ architecture struct of defender_sound_board is
  signal wram_do   : std_logic_vector( 7 downto 0);
  
  signal rom_cs    : std_logic;
+ signal spch_cs   : std_logic;
+
 -- signal rom_do    : std_logic_vector( 7 downto 0);
 
 -- pia port a
@@ -69,6 +73,9 @@ architecture struct of defender_sound_board is
 --      cb1 sound trigger (sel0-5 = 1)
 --      ca2 speech data N/C
 --      cb2 speech clock N/C
+
+ signal speech_cen : std_logic;
+ signal speech_data: std_logic;
 
  signal pia_rw_n   : std_logic;
  signal pia_cs     : std_logic;
@@ -86,8 +93,9 @@ reset_n   <= not reset;
 -- pia cs
 wram_cs <= '1' when cpu_addr(15 downto  8) = X"00" else '0';                       -- 0000-007F
 pia_cs  <= '1' when cpu_addr(14 downto 12) = "000" and cpu_addr(10) = '1' else '0'; -- 8400-8403 ? => 0400-0403
+spch_cs <= '1' when cpu_addr(15 downto 12) >= X"B" and cpu_addr(15 downto 12) <= X"E" else '0'; -- B000-EFFF
 rom_cs  <= '1' when cpu_addr(15 downto 12) = X"F" else '0';                        -- F800-FFFF
-	
+
 -- write enables
 wram_we <=    '1' when cpu_rw = '0' and wram_cs = '1' else '0';
 pia_rw_n <=   '0' when cpu_rw = '0' and pia_cs = '1' else '1'; 
@@ -96,7 +104,8 @@ pia_rw_n <=   '0' when cpu_rw = '0' and pia_cs = '1' else '1';
 cpu_di <=
 	wram_do when wram_cs = '1' else
 	pia_do  when pia_cs = '1' else
-	rom_do when rom_cs = '1' else X"55";
+	rom_do when rom_cs = '1' else
+	spch_do when spch_cs = '1' else X"55";
 
 -- pia I/O
 audio_out <= pia_pa_o;
@@ -139,7 +148,7 @@ port map(
 -- data => rom_do
 --);
 rom_vma   <= rom_cs and cpu_vma;
-rom_addr  <= cpu_addr(11 downto 0);
+rom_addr  <= (cpu_addr(13 downto 12) - "11") & cpu_addr(11 downto 0);
 
 -- cpu wram 
 cpu_ram : entity work.gen_ram
@@ -170,15 +179,25 @@ port map
 	pa_oe       => open,
 	ca1       	=> '1',
 	ca2_i      	=> '0',
-	ca2_o       => open,
+	ca2_o       => speech_data,
 	ca2_oe      => open,
 	pb_i      	=> pia_pb_i,
 	pb_o        => open,
 	pb_oe       => open,
 	cb1       	=> pia_cb1_i,
 	cb2_i      	=> '0',
-	cb2_o       => open,
+	cb2_o       => speech_cen,
 	cb2_oe      => open
+);
+
+-- CVSD speech decoder  
+IC1: entity work.HC55564  
+port map( 
+ clk => clk_0p89,
+ cen => speech_cen,
+ rst => '0', -- Reset is not currently implemented
+ bit_in => speech_data,
+ sample_out(15 downto 0) => speech_out
 );
 
 end struct;
