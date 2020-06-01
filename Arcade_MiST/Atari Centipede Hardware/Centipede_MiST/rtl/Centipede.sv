c525056fb3c06e1c06ebda9335ad476be3968fd6
Marcel <marceljenke@gmail.com>
2020-06-01 16:45:14 +0200

    Use Common Components

--- Arcade_MiST/Atari Centipede Hardware/Centipede_MiST/rtl/Centipede.sv
+++ Arcade_MiST/Atari Centipede Hardware/Centipede_MiST/rtl/Centipede.sv
@@ -1,8 +1,7 @@
 //============================================================================
 //  Arcade: Centipede
 //
-//  Port to MiST
-//  Copyright (C) 2018 Gehstock
+//  Port to MiST 2018 Gehstock
 //
 //  This program is free software; you can redistribute it and/or modify it
 //  under the terms of the GNU General Public License as published by the Free
@@ -42,12 +41,19 @@ module Centipede
 
 localparam CONF_STR = {
 	"Centipede;;",
-	"O1,Test,Off,On;",
 	"O2,Rotate Controls,Off,On;",
 	"O34,Scanlines,Off,25%,50%,75%;",
-	"T7,Reset;",
-	"V,v1.40.",`BUILD_DATE
+	"O5,Blend,Off,On;",
+	"O7,Test,Off,On;",
+	"T0,Reset;",
+	"V,v1.50.",`BUILD_DATE
 };
+
+wire       rotate    = status[2];
+wire [1:0] scanlines = status[4:3];
+wire       blend     = status[5];
+wire       joyswap   = status[6];
+wire       service   = status[7];
 
 assign LED = 1;
 assign AUDIO_R = AUDIO_L;
@@ -65,24 +71,26 @@ pll pll(
 wire [31:0] status;
 wire  [1:0] buttons;
 wire  [1:0] switches;
-wire  [7:0] joystick_0, joystick_1;
+wire  [7:0] joystick_0;
+wire  [7:0] joystick_1;
 wire        scandoublerD;
 wire        ypbpr;
-wire [10:0] ps2_key;
-wire  [6:0] audio1, audio2;
+wire        no_csync;
+wire        key_pressed;
+wire  [7:0] key_code;
+wire        key_strobe;
 wire	[7:0] RGB;
-wire 			vb, hb;
+wire 			hs, vs, vb, hb;
 wire 			blankn = ~(hb | vb);
-wire 			hs, vs;
 wire  [3:0] audio;
 
 centipede centipede(
 	.clk_100mhz(clk_100mhz),
 	.clk_12mhz(clk_12),
- 	.reset(status[0] | status[7] | buttons[1]),
-	.playerinput_i({ r_coin, c_coin, l_coin, m_test, m_cocktail, m_slam, m_start2, m_start1, m_fire2, m_fire1 }),
+ 	.reset(status[0] | buttons[1]),
+	.playerinput_i(~{ 1'b0, 1'b0, m_coin1, service, 1'b0, 1'b0, m_two_players, m_one_player, m_fireB, m_fireA }),
 	.trakball_i(),
-	.joystick_i({m_right , m_left, m_down, m_up, m_right , m_left, m_down, m_up}),
+	.joystick_i(~{m_right , m_left, m_down, m_up, m_right , m_left, m_down, m_up}),
 	.sw1_i("01010100"),
 	.sw2_i("00000000"),
 	.rgb_o(RGB),
@@ -92,28 +100,30 @@ centipede centipede(
 	.vblank_o(vb),
 	.audio_o(audio)
 	);
-
-mist_video #(.COLOR_DEPTH(3)) mist_video(
-	.clk_sys(clk_24),
-	.SPI_SCK(SPI_SCK),
-	.SPI_SS3(SPI_SS3),
-	.SPI_DI(SPI_DI),
-	.R(blankn ? RGB[2:0] : 0),
-	.G(blankn ? RGB[5:3] : 0),
-	.B(blankn ? RGB[7:6] : 0),
-	.HSync(hs),
-	.VSync(vs),
-	.VGA_R(VGA_R),
-	.VGA_G(VGA_G),
-	.VGA_B(VGA_B),
-	.VGA_VS(VGA_VS),
-	.VGA_HS(VGA_HS),
-	.rotate({1'b0,status[2]}),//(left/right,on/off)
-	.scandoubler_disable(scandoublerD),
-	.scanlines(status[4:3]),
-	.ypbpr(ypbpr)
-	);
-
+	
+mist_video #(.COLOR_DEPTH(3), .SD_HCNT_WIDTH(10)) mist_video(
+	.clk_sys        ( clk_24           ),
+	.SPI_SCK        ( SPI_SCK          ),
+	.SPI_SS3        ( SPI_SS3          ),
+	.SPI_DI         ( SPI_DI           ),
+	.R					 (blankn ? RGB[2:0] : 0),
+	.G					 (blankn ? RGB[5:3] : 0),
+	.B					 (blankn ? RGB[7:6] : 0),
+	.HSync          ( hs               ),
+	.VSync          ( vs               ),
+	.VGA_R          ( VGA_R            ),
+	.VGA_G          ( VGA_G            ),
+	.VGA_B          ( VGA_B            ),
+	.VGA_VS         ( VGA_VS           ),
+	.VGA_HS         ( VGA_HS           ),
+	.rotate         ( { 1'b0, rotate } ),
+	.ce_divider     ( 1'b1             ),
+	.blend          ( blend            ),
+	.scandoubler_disable(scandoublerD  ),
+	.no_csync       ( 1'b1             ),
+	.ypbpr          ( ypbpr            )
+	);	
+	
 user_io #(
 	.STRLEN(($size(CONF_STR)>>3)))
 user_io(
@@ -127,67 +137,42 @@ user_io(
 	.switches       (switches       ),
 	.scandoubler_disable (scandoublerD	  ),
 	.ypbpr          (ypbpr          ),
+	.no_csync       (no_csync       ),
 	.key_strobe     (key_strobe     ),
 	.key_pressed    (key_pressed    ),
 	.key_code       (key_code       ),
 	.joystick_0     (joystick_0     ),
 	.joystick_1     (joystick_1     ),
 	.status         (status         )
-	);
+	);	
 	
 dac #(
 	.C_bits(15))
 dac (
 	.clk_i(clk_24),
 	.res_n_i(1),
-	.dac_i({2{2'b0,audio,audio}}),
+	.dac_i({2{audio,audio}}),
 	.dac_o(AUDIO_L)
 	);
+	
+wire m_up, m_down, m_left, m_right, m_fireA, m_fireB, m_fireC, m_fireD, m_fireE, m_fireF;
+wire m_up2, m_down2, m_left2, m_right2, m_fire2A, m_fire2B, m_fire2C, m_fire2D, m_fire2E, m_fire2F;
+wire m_tilt, m_coin1, m_coin2, m_coin3, m_coin4, m_one_player, m_two_players, m_three_players, m_four_players;
 
-wire m_up     = status[2] ? ~btn_up & ~joystick_0[3] & ~joystick_1[3] : ~btn_right & ~joystick_0[0] & ~joystick_1[0];
-wire m_down   = status[2] ? ~btn_down & ~joystick_0[2] & ~joystick_1[2] : ~btn_left & ~joystick_0[1] & ~joystick_1[1];
-wire m_left   = status[2] ? ~btn_left & ~joystick_0[1] & ~joystick_1[1] : ~btn_up & ~joystick_0[3] & ~joystick_1[3];
-wire m_right  = status[2] ? ~btn_right & ~joystick_0[0] & ~joystick_1[0] : ~btn_down & ~joystick_0[2] & ~joystick_1[2];
-
-wire m_start1 = ~btn_one_player;
-wire m_start2 = 1'b1;
-wire m_fire1  = ~btn_fire1 & ~joystick_0[4] & ~joystick_1[4];
-wire m_fire2  = 1'b1;
-wire c_coin   = ~btn_coin;
-wire l_coin, r_coin = 1'b1;
-wire m_test = ~status[1];
-wire m_slam = 1'b1;//generate Noise
-wire m_cocktail = 1'b1;
-
-reg btn_one_player = 0;
-reg btn_two_players = 0;
-reg btn_left = 0;
-reg btn_right = 0;
-reg btn_down = 0;
-reg btn_up = 0;
-reg btn_fire1 = 0;
-reg btn_fire2 = 0;
-reg btn_fire3 = 0;
-reg btn_coin  = 0;
-wire       key_pressed;
-wire [7:0] key_code;
-wire       key_strobe;
-
-always @(posedge clk_24) begin
-	if(key_strobe) begin
-		case(key_code)
-			'h75: btn_up          <= key_pressed; // up
-			'h72: btn_down        <= key_pressed; // down
-			'h6B: btn_left        <= key_pressed; // left
-			'h74: btn_right       <= key_pressed; // right
-			'h76: btn_coin        <= key_pressed; // ESC
-			'h05: btn_one_player  <= key_pressed; // F1
-			'h06: btn_two_players <= key_pressed; // F2
-			'h14: btn_fire3       <= key_pressed; // ctrl
-			'h11: btn_fire2       <= key_pressed; // alt
-			'h29: btn_fire1       <= key_pressed; // Space
-		endcase
-	end
-end
+arcade_inputs inputs (
+	.clk         ( clk_24     ),
+	.key_strobe  ( key_strobe  ),
+	.key_pressed ( key_pressed ),
+	.key_code    ( key_code    ),
+	.joystick_0  ( joystick_0  ),
+	.joystick_1  ( joystick_1  ),
+	.rotate      ( rotate      ),
+	.orientation ( 2'b01		   ),
+	.joyswap     ( joyswap     ),
+	.oneplayer   ( 1'b1   		),
+	.controls    ( {m_tilt, m_coin4, m_coin3, m_coin2, m_coin1, m_four_players, m_three_players, m_two_players, m_one_player} ),
+	.player1     ( {m_fireF, m_fireE, m_fireD, m_fireC, m_fireB, m_fireA, m_up, m_down, m_left, m_right} ),
+	.player2     ( {m_fire2F, m_fire2E, m_fire2D, m_fire2C, m_fire2B, m_fire2A, m_up2, m_down2, m_left2, m_right2} )
+);	
 
 endmodule
