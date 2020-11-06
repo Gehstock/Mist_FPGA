3579e3d649058c2cb832019798d5514b9ead9c48
Gyorgy Szombathelyi <gyurco@freemail.hu>
2020-11-06 11:29:20 +0100

    Midway8080: Fix Boothill

--- Arcade_MiST/Midway-Taito 8080 Hardware/Boothill_MiST/rtl/BootHill_mist.sv
+++ Arcade_MiST/Midway-Taito 8080 Hardware/Boothill_MiST/rtl/BootHill_mist.sv
@@ -1,4 +1,4 @@
-module SpaceWalk_mist(
+module BootHill_mist(
 	output        LED,						
 	output  [5:0] VGA_R,
 	output  [5:0] VGA_G,
@@ -19,40 +19,45 @@ module SpaceWalk_mist(
 `include "rtl\build_id.v" 
 
 localparam CONF_STR = {
-	"Boot Hill;;",
+	"BOOTHILL;;",
 	"O34,Scanlines,Off,25%,50%,75%;",
-	"T6,Reset;",
+	"O6,Joystick swap,Off,On;",
+	"T0,Reset;",
 	"V,v1.20.",`BUILD_DATE
 };
+
+wire  [1:0] scanlines = status[4:3];
+wire        rotate = 0;
+wire        joyswap = status[6];
 
 assign LED = 1;
 assign AUDIO_R = AUDIO_L;
 
 
-wire clk_core, clk_sys;
+wire clk_sys, clk_vid;
 wire pll_locked;
 pll pll
 (
 	.inclk0(CLOCK_27),
 	.areset(),
-	.c0(clk_core),
-	.c1(clk_sys)
+	.c0(clk_sys),
+	.c1(clk_vid)
 );
 
 wire [31:0] status;
 wire  [1:0] buttons;
 wire  [1:0] switches;
-wire  [7:0] kbjoy;
 wire  [7:0] joystick_0,joystick_1;
 wire        scandoublerD;
 wire        ypbpr;
+wire        no_csync;
 wire        key_pressed;
 wire  [7:0] key_code;
 wire        key_strobe;
 wire  [7:0] audio;
-wire 			hsync,vsync;
-wire 			hs, vs;
-wire 			r,g,b;
+wire        hsync,vsync;
+wire        hs, vs;
+wire        r,g,b;
 
 wire [15:0]RAB;
 wire [15:0]AD;
@@ -67,17 +72,26 @@ wire RWE_n;
 wire Video;
 
 invaderst invaderst(
-	.Rst_n(~(status[0] | status[6] | buttons[1])),
-	.Clk(clk_core),
+	.Rst_n(~(status[0] | buttons[1])),
+	.Clk(clk_sys),
 	.ENA(),
-	.Coin(btn_coin),
-	.Sel1Player(btn_one_player),
-	.Sel2Player(btn_two_players),
-	.Fire(~m_fire),
-	.MoveLeft(~m_left),
-	.MoveRight(~m_right),
-	.MoveUp(~m_up),
-	.MoveDown(~m_down),	
+	.Coin(m_coin1 | m_coin2),
+	.Sel1Player(m_one_player),
+	.Fire1(m_fireA),
+	.Fire2(m_fire2A),
+	.GunUp1(m_fireB),
+	.GunDown1(m_fireC),
+	.MoveLeft1(m_left),
+	.MoveRight1(m_right),
+	.MoveUp1(m_up),
+	.MoveDown1(m_down),
+	.GunUp2(m_fire2B),
+	.GunDown2(m_fire2C),
+	.MoveLeft2(m_left2),
+	.MoveRight2(m_right2),
+	.MoveUp2(m_up2),
+	.MoveDown2(m_down2),
+//	.DIP(dip),
 	.RDB(RDB),
 	.IB(IB),
 	.RWD(RWD),
@@ -85,16 +99,15 @@ invaderst invaderst(
 	.AD(AD),
 	.SoundCtrl3(SoundCtrl3),
 	.SoundCtrl5(SoundCtrl5),
-	.SoundCtrl6(SoundCtrl6),
 	.Rst_n_s(Rst_n_s),
 	.RWE_n(RWE_n),
 	.Video(Video),
 	.HSync(hs),
 	.VSync(vs)
 	);
-		
+
 invaders_memory invaders_memory (
-	.Clock(clk_core),
+	.Clock(clk_sys),
 	.RW_n(RWE_n),
 	.Addr(AD),
 	.Ram_Addr(RAB),
@@ -102,24 +115,24 @@ invaders_memory invaders_memory (
 	.Ram_in(RWD),
 	.Rom_out(IB)
 	);
-		
+/*
 invaders_audio invaders_audio (
-	.Clk(clk_core),
+	.Clk(clk_sys),
 	.S0(SoundCtrl3 | SoundCtrl4),
 	.S1(SoundCtrl4),
 	.S2(SoundCtrl5 | SoundCtrl6),//hi
 	.S3(SoundCtrl6),//lo
 	.Aud(audio)
 	);
-
-mist_video #(.COLOR_DEPTH(3)) mist_video(
-	.clk_sys(clk_sys),
+*/
+mist_video #(.COLOR_DEPTH(1)) mist_video(
+	.clk_sys(clk_vid),
 	.SPI_SCK(SPI_SCK),
 	.SPI_SS3(SPI_SS3),
 	.SPI_DI(SPI_DI),
-	.R({Video,Video,Video}),
-	.G({Video,Video,Video}),
-	.B({Video,Video,Video}),
+	.R(Video),
+	.G(Video),
+	.B(Video),
 	.HSync(hs),
 	.VSync(vs),
 	.VGA_R(VGA_R),
@@ -129,8 +142,9 @@ mist_video #(.COLOR_DEPTH(3)) mist_video(
 	.VGA_HS(VGA_HS),
 	.ce_divider(0),
 	.scandoubler_disable(scandoublerD),
-	.scanlines(status[4:3]),
-	.ypbpr(ypbpr)
+	.scanlines(scanlines),
+	.ypbpr(ypbpr),
+	.no_csync(no_csync)
 	);
 
 user_io #(
@@ -146,6 +160,7 @@ user_io(
 	.switches       (switches       ),
 	.scandoubler_disable (scandoublerD	  ),
 	.ypbpr          (ypbpr          ),
+	.no_csync       (no_csync       ),
 	.key_strobe     (key_strobe     ),
 	.key_pressed    (key_pressed    ),
 	.key_code       (key_code       ),
@@ -161,38 +176,24 @@ dac dac (
 	.dac_o(AUDIO_L)
 	);
 
-wire m_up     = btn_up | joystick_0[3] | joystick_1[3];
-wire m_down   = btn_down | joystick_0[2] | joystick_1[2];
-wire m_left   = btn_left | joystick_0[1] | joystick_1[1];
-wire m_right  = btn_right | joystick_0[0] | joystick_1[0];
-wire m_fire   = btn_fire1 | joystick_0[4] | joystick_1[4];
-wire m_bomb   = btn_fire2 | joystick_0[5] | joystick_1[5];
-reg btn_one_player = 0;
-reg btn_two_players = 0;
-reg btn_left = 0;
-reg btn_right = 0;
-reg btn_down = 0;
-reg btn_up = 0;
-reg btn_fire1 = 0;
-reg btn_fire2 = 0;
-reg btn_fire3 = 0;
-reg btn_coin  = 0;
+wire m_up, m_down, m_left, m_right, m_fireA, m_fireB, m_fireC, m_fireD, m_fireE, m_fireF;
+wire m_up2, m_down2, m_left2, m_right2, m_fire2A, m_fire2B, m_fire2C, m_fire2D, m_fire2E, m_fire2F;
+wire m_tilt, m_coin1, m_coin2, m_coin3, m_coin4, m_one_player, m_two_players, m_three_players, m_four_players;
 
-always @(posedge clk_sys) begin
-	if(key_strobe) begin
-		case(key_code)
-			'h75: btn_up          <= key_pressed; // up
-			'h72: btn_down        <= key_pressed; // down
-			'h6B: btn_left        <= key_pressed; // left
-			'h74: btn_right       <= key_pressed; // right
-			'h76: btn_coin        <= key_pressed; // ESC
-			'h05: btn_one_player  <= key_pressed; // F1
-			'h06: btn_two_players <= key_pressed; // F2
-			'h14: btn_fire3       <= key_pressed; // ctrl
-			'h11: btn_fire2       <= key_pressed; // alt
-			'h29: btn_fire1       <= key_pressed; // Space
-		endcase
-	end
-end
+arcade_inputs inputs (
+	.clk         ( clk_sys     ),
+	.key_strobe  ( key_strobe  ),
+	.key_pressed ( key_pressed ),
+	.key_code    ( key_code    ),
+	.joystick_0  ( joystick_0  ),
+	.joystick_1  ( joystick_1  ),
+	.rotate      ( rotate      ),
+	.orientation ( 2'b00       ),
+	.joyswap     ( joyswap     ),
+	.oneplayer   ( 1'b0        ),
+	.controls    ( {m_tilt, m_coin4, m_coin3, m_coin2, m_coin1, m_four_players, m_three_players, m_two_players, m_one_player} ),
+	.player1     ( {m_fireF, m_fireE, m_fireD, m_fireC, m_fireB, m_fireA, m_up, m_down, m_left, m_right} ),
+	.player2     ( {m_fire2F, m_fire2E, m_fire2D, m_fire2C, m_fire2B, m_fire2A, m_up2, m_down2, m_left2, m_right2} )
+);
 
 endmodule
