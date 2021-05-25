b84aa1998402e75548bc2e997a475867f7bc16de
Marcel <marceljenke@gmail.com>
2021-05-25 14:32:30 +0200

    Add Number Crash on Pacman Hardware

---
+++ Arcade_MiST/Namco Pacman Hardware/NumberCrash_MiST/rtl/NumberCrash_MiST.sv
@@ -0,0 +1,182 @@
+//============================================================================
+//  Arcade: Number Crash
+//
+//  Version for MiSTer
+//  Copyright (C) 2021 Gehstock
+//
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License as published by the Free
+//  Software Foundation; either version 2 of the License, or (at your option)
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//============================================================================
+
+module NumberCrash_MiST(
+	output        LED,						
+	output  [5:0] VGA_R,
+	output  [5:0] VGA_G,
+	output  [5:0] VGA_B,
+	output        VGA_HS,
+	output        VGA_VS,
+	output        AUDIO_L,
+	output        AUDIO_R,	
+	input         SPI_SCK,
+	output        SPI_DO,
+	input         SPI_DI,
+	input         SPI_SS2,
+	input         SPI_SS3,
+	input         CONF_DATA0,
+	input         CLOCK_27
+);
+
+`include "rtl\build_id.v"
+
+localparam CONF_STR = {
+	"NumberCrash;;",
+	"O2,Rotate Controls,Off,On;",
+	"O34,Scanlines,Off,25%,50%,75%;",
+	"O5,Blend,Off,On;",
+	"O6,Rack Test (Cheat),Off,On;",
+	"T0,Reset;",
+	"V,v1.20.",`BUILD_DATE
+};
+
+assign LED = 1;
+assign AUDIO_R = AUDIO_L;
+
+wire clk_sys, clk_snd;
+wire pll_locked;
+pll pll(
+	.inclk0(CLOCK_27),
+	.areset(0),
+	.c0(clk_sys),
+	.locked(pll_locked)
+	);
+
+reg ce_6m;
+always @(posedge clk_sys) begin
+	reg [1:0] div;
+	div <= div + 1'd1;
+	ce_6m <= !div;
+end
+
+wire [63:0] status;
+wire  [1:0] buttons;
+wire  [1:0] switches;
+wire [31:0] joystick_0;
+wire [31:0] joystick_1;
+wire        scandoublerD;
+wire        ypbpr;
+wire  [7:0] audio;
+wire 			hs, vs;
+wire 			hb, vb;
+wire        blankn = ~(hb | vb);
+wire  [2:0] r,g;
+wire  [1:0] b;
+wire        no_csync;
+wire       	key_pressed;
+wire  [7:0] key_code;
+wire       	key_strobe;
+wire			custom = 1'b1;//todo
+
+pacmant pacmant(
+	.O_VIDEO_R(r),
+   .O_VIDEO_G(g),
+   .O_VIDEO_B(b),
+   .O_HSYNC(hs),
+   .O_VSYNC(vs),
+   .O_HBLANK(hb),
+   .O_VBLANK(vb),
+	.O_AUDIO(audio),
+	.in0(~{1'b0, custom, 1'b0, status[6], m_down, m_left, m_right, m_up}),
+   .in1(~{1'b1,m_one_player, m_coin1, 4'b1111, m_fireA}),
+   .dipsw(~{8'b11111111}),//todo later
+   .RESET(status[0] | buttons[1]),
+	.CLK(clk_sys),
+   .ENA_6(ce_6m)
+	);
+
+mist_video #(.COLOR_DEPTH(3),.SD_HCNT_WIDTH(10)) mist_video(
+	.clk_sys(clk_sys),
+	.SPI_SCK(SPI_SCK),
+	.SPI_SS3(SPI_SS3),
+	.SPI_DI(SPI_DI),
+	.R(blankn ? r : 0),
+	.G(blankn ? g : 0),
+	.B(blankn ? b : 0),
+	.HSync(~hs),
+	.VSync(~vs),
+	.VGA_R(VGA_R),
+	.VGA_G(VGA_G),
+	.VGA_B(VGA_B),
+	.VGA_VS(VGA_VS),
+	.VGA_HS(VGA_HS),
+	.rotate({1'b1,status[2]}),
+	.scandoubler_disable(scandoublerD),
+	.scanlines(status[4:3]),
+	.ce_divider(1'b1),
+	.blend(status[5]),
+	.no_csync(no_csync),
+	.ypbpr(ypbpr)
+	);
+
+user_io #(
+	.STRLEN(($size(CONF_STR)>>3)))
+user_io(
+	.clk_sys        (clk_sys        ),
+	.conf_str       (CONF_STR       ),
+	.SPI_CLK        (SPI_SCK        ),
+	.SPI_SS_IO      (CONF_DATA0     ),
+	.SPI_MISO       (SPI_DO         ),
+	.SPI_MOSI       (SPI_DI         ),
+	.buttons        (buttons        ),
+	.switches       (switches       ),
+	.scandoubler_disable (scandoublerD	  ),
+	.ypbpr          (ypbpr          ),
+	.key_strobe     (key_strobe     ),
+	.key_pressed    (key_pressed    ),
+	.key_code       (key_code       ),
+	.joystick_0     (joystick_0     ),
+	.joystick_1     (joystick_1     ),
+	.status         (status         )
+	);
+	
+dac #(
+	.C_bits(8))
+dac(
+	.clk_i(clk_sys),
+	.res_n_i(1),
+	.dac_i(audio),
+	.dac_o(AUDIO_L)
+	);
+
+wire m_up, m_down, m_left, m_right, m_fireA, m_fireB, m_fireC, m_fireD, m_fireE, m_fireF;
+wire m_up2, m_down2, m_left2, m_right2, m_fire2A, m_fire2B, m_fire2C, m_fire2D, m_fire2E, m_fire2F;
+wire m_tilt, m_coin1, m_coin2, m_coin3, m_coin4, m_one_player, m_two_players, m_three_players, m_four_players;
+
+arcade_inputs inputs (
+	.clk         ( clk_sys    ),
+	.key_strobe  ( key_strobe  ),
+	.key_pressed ( key_pressed ),
+	.key_code    ( key_code    ),
+	.joystick_0  ( joystick_0  ),
+	.joystick_1  ( joystick_1  ),
+	.rotate      ( status[2]   ),
+	.orientation ( 2'b10       ),
+//	.joyswap     ( 1'b0        ),
+	.oneplayer   ( 1'b1        ),
+	.controls    ( {m_tilt, m_coin4, m_coin3, m_coin2, m_coin1, m_four_players, m_three_players, m_two_players, m_one_player} ),
+	.player1     ( {m_fireF, m_fireE, m_fireD, m_fireC, m_fireB, m_fireA, m_up, m_down, m_left, m_right} ),
+	.player2     ( {m_fire2F, m_fire2E, m_fire2D, m_fire2C, m_fire2B, m_fire2A, m_up2, m_down2, m_left2, m_right2} )
+);
+
+
+endmodule 
---
+++ Arcade_MiST/Namco Pacman Hardware/NumberCrash_MiST/rtl/pacman_video.vhd
@@ -0,0 +1,360 @@
+--
+-- A simulation model of Pacman hardware
+-- Copyright (c) MikeJ - January 2006
+--
+-- All rights reserved
+--
+-- Redistribution and use in source and synthezised forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- Redistributions of source code must retain the above copyright notice,
+-- this list of conditions and the following disclaimer.
+--
+-- Redistributions in synthesized form must reproduce the above copyright
+-- notice, this list of conditions and the following disclaimer in the
+-- documentation and/or other materials provided with the distribution.
+--
+-- Neither the name of the author nor the names of other contributors may
+-- be used to endorse or promote products derived from this software without
+-- specific prior written permission.
+--
+-- THIS CODE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+-- THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+-- PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE
+-- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+-- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+-- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+-- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+-- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+-- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+-- POSSIBILITY OF SUCH DAMAGE.
+--
+-- You are responsible for any legal issues arising from your use of this code.
+--
+-- The latest version of this file can be found at: www.fpgaarcade.com
+--
+-- Email pacman@fpgaarcade.com
+--
+-- Revision list
+--
+-- version 003 Jan 2006 release, general tidy up
+-- version 001 initial release
+--
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.std_logic_unsigned.all;
+  use ieee.numeric_std.all;
+
+library UNISIM;
+
+entity PACMAN_VIDEO is
+  port (
+    I_HCNT            : in    std_logic_vector(8 downto 0);
+    I_VCNT            : in    std_logic_vector(8 downto 0);
+    --
+    I_AB              : in    std_logic_vector(11 downto 0);
+    I_DB              : in    std_logic_vector( 7 downto 0);
+    --
+    I_HBLANK          : in    std_logic;
+    I_VBLANK          : in    std_logic;
+    I_FLIP            : in    std_logic;
+    I_WR2_L           : in    std_logic;
+    --
+    O_RED             : out   std_logic_vector(2 downto 0);
+    O_GREEN           : out   std_logic_vector(2 downto 0);
+    O_BLUE            : out   std_logic_vector(1 downto 0);
+    ENA_6             : in    std_logic;
+    CLK               : in    std_logic
+    );
+end;
+
+architecture RTL of PACMAN_VIDEO is
+
+  signal sprite_xy_ram_temp : std_logic_vector(7 downto 0);
+  signal dr                 : std_logic_vector(7 downto 0);
+
+  signal char_reg           : std_logic_vector(7 downto 0);
+  signal char_sum_reg       : std_logic_vector(3 downto 0);
+  signal char_match_reg     : std_logic;
+  signal char_hblank_reg    : std_logic;
+  signal char_hblank_reg_t1 : std_logic;
+  signal db_reg             : std_logic_vector(7 downto 0);
+
+  signal xflip              : std_logic;
+  signal yflip              : std_logic;
+  signal obj_on             : std_logic;
+
+  signal ca                 : std_logic_vector(12 downto 0);
+  signal char_rom_5ef_dout  : std_logic_vector(7 downto 0);
+
+  signal shift_regl         : std_logic_vector(3 downto 0);
+  signal shift_regu         : std_logic_vector(3 downto 0);
+  signal shift_op           : std_logic_vector(1 downto 0);
+  signal shift_sel          : std_logic_vector(1 downto 0);
+
+  signal vout_obj_on        : std_logic;
+  signal vout_yflip         : std_logic;
+  signal vout_hblank        : std_logic;
+  signal vout_db            : std_logic_vector(4 downto 0);
+
+  signal cntr_ld            : std_logic;
+  signal ra                 : std_logic_vector(7 downto 0);
+  signal sprite_ram_ip      : std_logic_vector(3 downto 0);
+  signal sprite_ram_op      : std_logic_vector(3 downto 0);
+  signal sprite_ram_addr    : std_logic_vector(7 downto 0);
+  signal sprite_ram_addr_t1 : std_logic_vector(7 downto 0);
+  signal vout_obj_on_t1     : std_logic;
+  signal col_rom_addr       : std_logic_vector(7 downto 0);
+
+  signal lut_4a             : std_logic_vector(7 downto 0);
+  signal lut_4a_t1          : std_logic_vector(7 downto 0);
+  signal vout_hblank_t1     : std_logic;
+  signal sprite_ram_reg     : std_logic_vector(3 downto 0);
+
+  signal video_out          : std_logic_vector(7 downto 0);
+  signal video_op_sel       : std_logic;
+  signal final_col          : std_logic_vector(3 downto 0);
+
+begin
+
+	-- ram enable is low when HBLANK_L is 0 (for sprite access) or
+	-- 2H is low (for cpu writes)
+	-- we can simplify this
+	dr <= not sprite_xy_ram_temp when I_HBLANK = '1' else "11111111"; -- pull ups on board
+	
+	sprite_xy_ram : work.dpram generic map (4,8)
+	port map
+	(
+		clk_a_i  => CLK,
+		en_a_i   => ENA_6,
+		we_i     => not I_WR2_L,
+		addr_a_i => I_AB(3 downto 0),
+		data_a_i => I_DB,
+
+		clk_b_i  => CLK,
+		addr_b_i => I_AB(3 downto 0),
+		data_b_o => sprite_xy_ram_temp
+	);
+
+	p_char_regs : process
+		variable inc : std_logic;
+		variable sum : std_logic_vector(8 downto 0);
+		variable match : std_logic;
+	begin
+		wait until rising_edge (CLK);
+			if (I_HCNT(2 downto 0) = "011") and (ENA_6 = '1') then  -- rising 4h
+			inc := (not I_HBLANK);
+
+			-- 1f, 2f
+			sum := (I_VCNT(7 downto 0) & '1') + (dr & inc);
+
+			-- 3e
+			match := '0';
+
+			if (sum(8 downto 5) = "1111") then
+			  match := '1';
+			end if;
+
+			-- 1h
+			char_sum_reg     <= sum(4 downto 1);
+			char_match_reg   <= match;
+			char_hblank_reg  <= I_HBLANK;
+
+			-- 4d
+			db_reg <= I_DB; -- character reg
+		end if;
+	end process;
+
+	p_flip_comb : process(char_hblank_reg, I_FLIP, db_reg)
+	begin
+		if (char_hblank_reg = '0') then
+			xflip     <= I_FLIP;
+			yflip     <= I_FLIP;
+		else
+			xflip     <= db_reg(1);
+			yflip     <= db_reg(0);
+		end if;
+	end process;
+
+	p_char_addr_comb : process(db_reg, I_HCNT,
+                             char_match_reg, char_sum_reg, char_hblank_reg,
+                             xflip, yflip)
+	begin
+		-- 2h, 4e
+		obj_on <= char_match_reg or I_HCNT(8); -- 256h not 256h_l
+
+		ca(12) <= char_hblank_reg;
+		ca(11 downto 6) <= db_reg(7 downto 2);
+
+		if (char_hblank_reg = '0') then
+			ca(5)     <= db_reg(1);
+			ca(4)     <= db_reg(0);
+		else
+			ca(5)     <= char_sum_reg(3) xor xflip;
+			ca(4)     <= I_HCNT(3);
+		end if;
+
+		ca(3) <= I_HCNT(2)       xor yflip;
+		ca(2) <= char_sum_reg(2) xor xflip;
+		ca(1) <= char_sum_reg(1) xor xflip;
+		ca(0) <= char_sum_reg(0) xor xflip;
+	end process;
+
+	-- char roms
+	char_rom_5ef : entity work.GFX1
+	port map (
+		CLK         => CLK,
+		ADDR        => ca,
+		DATA        => char_rom_5ef_dout
+	);
+
+	p_char_shift : process
+	begin
+		-- 4 bit shift req
+		wait until rising_edge (CLK);
+		if (ENA_6 = '1') then
+			case shift_sel is
+				when "00" =>	null;
+
+				when "01" =>	shift_regu <= '0' & shift_regu(3 downto 1);
+									shift_regl <= '0' & shift_regl(3 downto 1);
+
+				when "10" =>	shift_regu <= shift_regu(2 downto 0) & '0';
+									shift_regl <= shift_regl(2 downto 0) & '0';
+
+				when "11" =>	shift_regu <= char_rom_5ef_dout(7 downto 4); -- load
+									shift_regl <= char_rom_5ef_dout(3 downto 0);
+				when others => null;
+			end case;
+		end if;
+	end process;
+
+	p_char_shift_comb : process(I_HCNT, vout_yflip, shift_regu, shift_regl)
+		variable ip : std_logic;
+	begin
+		ip := I_HCNT(0) and I_HCNT(1);
+		if (vout_yflip = '0') then
+
+			shift_sel(0) <= ip;
+			shift_sel(1) <= '1';
+			shift_op(0) <= shift_regl(3);
+			shift_op(1) <= shift_regu(3);
+		else
+
+			shift_sel(0) <= '1';
+			shift_sel(1) <= ip;
+			shift_op(0) <= shift_regl(0);
+			shift_op(1) <= shift_regu(0);
+		end if;
+	end process;
+
+	p_video_out_reg : process
+	begin
+		wait until rising_edge (CLK);
+		if (ENA_6 = '1') then
+			if (I_HCNT(2 downto 0) = "111") then
+				vout_obj_on   <= obj_on;
+				vout_yflip    <= yflip;
+				vout_hblank   <= I_HBLANK;
+				vout_db(4 downto 0) <= I_DB(4 downto 0); -- colour reg
+			end if;
+		end if;
+	end process;
+
+	p_lut_4a_comb : process(vout_db, shift_op)
+	begin
+		col_rom_addr <= '0' & vout_db(4 downto 0) & shift_op(1 downto 0);
+	end process;
+
+	col_rom_4a : entity work.PROM4_DST
+	port map (
+		CLK         => CLK,
+		ADDR        => col_rom_addr,
+		DATA        => lut_4a
+	);
+
+	cntr_ld <= '1' when (I_HCNT(3 downto 0) = "0111") and (vout_hblank='1' or vout_obj_on='0') else '0';
+
+	p_ra_cnt : process
+	begin
+		wait until rising_edge (CLK);
+		if (ENA_6 = '1') then
+			if (cntr_ld = '1') then
+				ra <= dr;
+			else
+				ra <= ra + "1";
+			end if;
+		end if;
+	end process;
+
+	sprite_ram_addr <= ra;
+
+	u_sprite_ram : work.dpram generic map (8,4)
+	port map
+	(
+		clk_a_i  => CLK,
+		en_a_i   => ENA_6,
+		we_i     => vout_obj_on,
+		addr_a_i => sprite_ram_addr,
+		data_a_i => sprite_ram_ip,
+
+		clk_b_i  => CLK,
+		addr_b_i => sprite_ram_addr,
+		data_b_o => sprite_ram_op
+	);
+
+	sprite_ram_reg <= sprite_ram_op when vout_obj_on_t1 = '1' else "0000";
+	video_op_sel <= '1' when not (sprite_ram_reg = "0000") else '0';
+
+	p_sprite_ram_ip_reg : process
+	begin
+		wait until rising_edge (CLK);
+		if (ENA_6 = '1') then
+			vout_obj_on_t1 <= vout_obj_on;
+			vout_hblank_t1 <= vout_hblank;
+			lut_4a_t1 <= lut_4a;
+		end if;
+	end process;
+
+	p_sprite_ram_ip_comb : process(vout_hblank_t1, video_op_sel, sprite_ram_reg, lut_4a_t1)
+	begin
+	-- 3a
+		if (vout_hblank_t1 = '0') then
+			sprite_ram_ip <= (others => '0');
+		else
+			if (video_op_sel = '1') then
+				sprite_ram_ip <= sprite_ram_reg;
+			else
+				sprite_ram_ip <= lut_4a_t1(3 downto 0);
+			end if;
+		end if;
+	end process;
+
+	p_video_op_comb : process(vout_hblank, I_VBLANK, video_op_sel, sprite_ram_reg, lut_4a)
+	begin
+		-- 3b
+		if (vout_hblank = '1') or (I_VBLANK = '1') then
+			final_col <= (others => '0');
+		else
+			if (video_op_sel = '1') then
+				final_col <= sprite_ram_reg; -- sprite
+			else
+				final_col <= lut_4a(3 downto 0);
+			end if;
+		end if;
+	end process;
+
+	col_rom_7f : entity work.PROM7_DST
+	port map (
+		CLK         => CLK,
+		ADDR        => final_col,
+		DATA        => video_out
+	);
+
+	--  assign outputs
+	O_BLUE (1 downto 0) <= video_out(7 downto 6);
+	O_GREEN(2 downto 0) <= video_out(5 downto 3);
+	O_RED  (2 downto 0) <= video_out(2 downto 0);
+
+end architecture;
