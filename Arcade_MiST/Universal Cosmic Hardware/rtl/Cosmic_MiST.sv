745ff6a00d786fb4e1316dee1d179a5cd9d43b1e
Gyorgy Szombathelyi <gyurco@freemail.hu>
2021-12-12 19:07:52 +0100

    Universal Cosmic HW by Mike Coates port

---
+++ Arcade_MiST/Universal Cosmic Hardware/rtl/Cosmic_MiST.sv
@@ -0,0 +1,407 @@
+//  Arcade: Universal Cosmic series (Z80 version)
+//
+//  Mike Coates.
+//
+//  MiST port by slingshot
+//
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License as published by the Free
+//  Software Foundation; either version 2 of the License, or (at your option)
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//============================================================================
+
+module Cosmic_MiST(
+	output        LED,
+	output  [5:0] VGA_R,
+	output  [5:0] VGA_G,
+	output  [5:0] VGA_B,
+	output        VGA_HS,
+	output        VGA_VS,
+	output        AUDIO_L,
+	output        AUDIO_R,
+	input         SPI_SCK,
+	output        SPI_DO,
+	input         SPI_DI,
+	input         SPI_SS2,
+	input         SPI_SS3,
+	input         CONF_DATA0,
+	input         CLOCK_27,
+
+	output [12:0] SDRAM_A,
+	inout  [15:0] SDRAM_DQ,
+	output        SDRAM_DQML,
+	output        SDRAM_DQMH,
+	output        SDRAM_nWE,
+	output        SDRAM_nCAS,
+	output        SDRAM_nRAS,
+	output        SDRAM_nCS,
+	output  [1:0] SDRAM_BA,
+	output        SDRAM_CLK,
+	output        SDRAM_CKE
+);
+
+`include "rtl/build_id.v" 
+
+`define CORE_NAME "COSMIIEN"
+
+localparam CONF_STR = {
+	`CORE_NAME,";;",
+	"O2,Rotate Controls,Off,On;",
+	"O34,Scanlines,Off,25%,50%,75%;",
+	"O5,Blend,Off,On;",
+	"O6,Swap Joysticks,Off,On;",
+	"O7,Flip,Off,On;",
+	"DIP;",
+	"T0,Reset;",
+	"V,v1.0.",`BUILD_DATE
+};
+
+wire       rotate    = status[2];
+wire [1:0] scanlines = status[4:3];
+wire       blend     = status[5];
+wire       joyswap   = status[6];
+wire       flip      = status[7];
+
+wire  [6:0] core_mod;
+wire  [1:0] orientation = {flip, 1'b1}; // [left/right, landscape/portrait]
+
+wire clk_vid, clk_sys;
+wire pll_locked;
+pll_mist pll(
+	.inclk0(CLOCK_27),
+	.areset(0),
+	.c0(clk_vid),//43.264
+	.c1(clk_sys),//10.816
+	.locked(pll_locked)
+	);
+
+assign SDRAM_CLK = clk_vid;
+assign LED = ~ioctl_downl;
+
+wire pix_clk = clk_div[0];      // Pixel clock = 5.408 Mhz
+
+reg  [1:0] clk_div = 2'd0;      // Clock divider (for Pixel and CPU speed 2.7Mhz)
+reg  [2:0] clk_div2 = 3'd0;     // Clock divider (for CPU speed 1.8 Mhz)
+reg cpu_ena_27;                 // 2.7 Mhz
+reg cpu_ena_18;                 // 1.8 Mhz
+
+// Divider for other clocks (7474 and 74161 on PCB)
+always @(posedge clk_sys) begin
+	cpu_ena_27 <= 1'd0;
+	cpu_ena_18 <= 1'd0;
+        
+	clk_div <= clk_div + 1'b1;
+	clk_div2 <= clk_div2 + 1'b1;
+
+	// cpu clocks
+	if (clk_div == 3) cpu_ena_27 <= 1'd1;
+                        
+	if (clk_div2 == 5) begin
+		cpu_ena_18 <= 1'd1;
+		clk_div2 <= 3'd0;
+	end
+end
+
+// Game ID - CPU Speed
+// 01 = Space Panic - 1.8 Mhz
+// 02 = Magic Spot  - 2.7 Mhz
+// 03 = Cosmic Alien - 1.8 Mhz
+// 04 = Devil Zone - 2.7 Mhz
+// 05 = No Mans Land - 1.8Mhz
+
+wire cpu_ena = (core_mod==2 || core_mod==4) ? cpu_ena_27 : cpu_ena_18;
+
+wire [15:0] dip = status[23:8];
+// Panic
+wire  [7:0] Panic_P1 = {~m_fireB,2'd3,~m_up,~m_down,~m_left,~m_right,~m_fireA};
+wire  [7:0] Panic_P2 = {~m_fire2B,2'd3,~m_up2,~m_down2,~m_left2,~m_right2,~m_fire2A};
+wire  [7:0] Panic_P3 = {1'd1,~m_coin1,4'D15,~m_two_players,~m_one_player};
+// Magical Spot
+wire  [7:0] MagSpot_P1 = {dip[15:14],~m_right,3'd7,~m_left,1'd1}; // Includes bonus dips
+wire  [7:0] MagSpot_P2 = {2'd3,~m_right2,3'd7,~m_left2,1'd1};
+wire  [7:0] MagSpot_P3 = {~m_fireA,~m_fire2A,5'D31,~vblank};
+wire  [7:0] MagSpot_P4 = {~m_one_player,~m_two_players,dip[5:0]};
+// Cosmic Alien
+wire  [7:0] Alien_P1 = {5'd31,~m_left,~m_right,~m_fireA};
+wire  [7:0] Alien_P2 = {5'd31,~m_left2,~m_right2,~m_fire2A};
+wire  [7:0] Alien_P3 = {2'd0,VCount[7:2]};
+
+// Select correct inputs
+wire  [7:0] IN0 = (core_mod==1)? Panic_P1 : (core_mod==2 || core_mod==4 || core_mod==5)? MagSpot_P1 : Alien_P1;
+wire  [7:0] IN1 = (core_mod==1)? Panic_P2 : (core_mod==2 || core_mod==4 || core_mod==5)? MagSpot_P2 : Alien_P2;
+wire  [7:0] IN2 = (core_mod==1)? Panic_P3 : (core_mod==2 || core_mod==4 || core_mod==5)? MagSpot_P3 : Alien_P3;
+wire  [7:0] DIP = (core_mod==1)? dip[7:0] : MagSpot_P4;
+
+
+wire [63:0] status;
+wire  [1:0] buttons;
+wire  [1:0] switches;
+wire [19:0] joystick_0;
+wire [19:0] joystick_1;
+wire        scandoublerD;
+wire        no_csync;
+wire        ypbpr;
+wire        key_pressed;
+wire  [7:0] key_code;
+wire        key_strobe;
+
+wire        mouse_strobe;
+wire signed [8:0] mouse_x;
+wire signed [8:0] mouse_y;
+wire  [7:0] mouse_flags;
+wire        mouse_idx;
+
+user_io #(
+	.STRLEN($size(CONF_STR)>>3))
+user_io(
+	.clk_sys        ( clk_sys          ),
+	.conf_str       ( CONF_STR         ),
+	.SPI_CLK        ( SPI_SCK          ),
+	.SPI_SS_IO      ( CONF_DATA0       ),
+	.SPI_MISO       ( SPI_DO           ),
+	.SPI_MOSI       ( SPI_DI           ),
+	.buttons        ( buttons          ),
+	.switches       ( switches         ),
+	.scandoubler_disable (scandoublerD ),
+	.ypbpr          ( ypbpr            ),
+	.no_csync       ( no_csync         ),
+	.core_mod       ( core_mod         ),
+	.key_strobe     ( key_strobe       ),
+	.key_pressed    ( key_pressed      ),
+	.key_code       ( key_code         ),
+	.mouse_idx      ( mouse_idx        ),
+	.mouse_strobe   ( mouse_strobe     ),
+	.mouse_x        ( mouse_x          ),
+	.mouse_y        ( mouse_y          ),
+	.mouse_flags    ( mouse_flags      ),
+	.joystick_0     ( joystick_0       ),
+	.joystick_1     ( joystick_1       ),
+	.status         ( status           )
+	);
+
+wire        ioctl_downl;
+wire        ioctl_upl;
+wire  [7:0] ioctl_index;
+wire        ioctl_wr;
+wire [24:0] ioctl_addr;
+wire  [7:0] ioctl_dout;
+wire  [7:0] ioctl_din;
+
+data_io data_io (
+	.clk_sys       ( clk_sys      ),
+	.SPI_SCK       ( SPI_SCK      ),
+	.SPI_SS2       ( SPI_SS2      ),
+	.SPI_DI        ( SPI_DI       ),
+	.SPI_DO        ( SPI_DO       ),
+	.ioctl_download( ioctl_downl  ),
+	.ioctl_upload  ( ioctl_upl    ),
+	.ioctl_index   ( ioctl_index  ),
+	.ioctl_wr      ( ioctl_wr     ),
+	.ioctl_addr    ( ioctl_addr   ),
+	.ioctl_dout    ( ioctl_dout   ),
+	.ioctl_din     ( ioctl_din    )
+);
+
+reg reset = 1;
+reg rom_loaded = 0;
+always @(posedge clk_sys) begin
+	reg ioctl_downlD;
+	ioctl_downlD <= ioctl_downl;
+
+	if (ioctl_downlD & ~ioctl_downl) rom_loaded <= 1;
+	reset <= status[0] | buttons[1] | ioctl_downl | ~rom_loaded;
+end
+
+wire        audio;
+wire        hblank, vblank;
+wire        hs, vs;
+wire  [3:0] r,g,b;
+wire  [8:0] VCount;
+wire        blank = hblank | vblank;
+
+COSMIC COSMIC
+(
+	.O_VIDEO_R(r),
+	.O_VIDEO_G(g),
+	.O_VIDEO_B(b),
+	.O_HSYNC(hs),
+	.O_VSYNC(vs),
+	.O_HBLANK(hblank),
+	.O_VBLANK(vblank),
+	.I_H_OFFSET(),
+	.I_V_OFFSET(),
+	.I_FLIP(flip),
+	.O_VCOUNT(VCount),
+
+	.dn_addr(ioctl_addr[15:0]),
+	.dn_data(ioctl_dout),
+	.dn_wr(ioctl_wr && (ioctl_index == 0) && ioctl_addr < table_offset),
+	.dn_ld(ioctl_downl),
+
+	.O_SoundPort(SoundTrigger),
+	.O_SoundStop(SoundStop),
+	.O_AUDIO(audio),
+	.O_Sound_EN(),
+
+	.dipsw1(DIP),
+	.dipsw2(dip[15:8]),
+	.in0(IN0),
+	.in1(IN1),
+	.in2(IN2),
+	.coin(m_coin1),
+
+	.RESET(reset),
+	.PIX_CLK(pix_clk),
+	.CPU_ENA(cpu_ena),
+	.CLK(clk_sys),
+	.GAME(core_mod),
+
+	.PAUSED(),
+
+	.hs_address(),
+	.hs_data_out(),
+	.hs_data_in(),
+	.hs_write(),
+	.hs_access()
+);
+
+mist_video #(.COLOR_DEPTH(4), .SD_HCNT_WIDTH(11)) mist_video(
+	.clk_sys        ( clk_vid          ),
+	.SPI_SCK        ( SPI_SCK          ),
+	.SPI_SS3        ( SPI_SS3          ),
+	.SPI_DI         ( SPI_DI           ),
+	.R              ( blank ? 4'd0 : r ),
+	.G              ( blank ? 4'd0 : g ),
+	.B              ( blank ? 4'd0 : b ),
+	.HSync          ( hs               ),
+	.VSync          ( vs               ),
+	.VGA_R          ( VGA_R            ),
+	.VGA_G          ( VGA_G            ),
+	.VGA_B          ( VGA_B            ),
+	.VGA_VS         ( VGA_VS           ),
+	.VGA_HS         ( VGA_HS           ),
+	.rotate         ( {flip,rotate}    ),
+	.scandoubler_disable( scandoublerD ),
+	.ce_divider     ( 1'b1             ),
+	.no_csync       ( no_csync         ),
+	.scanlines      ( scanlines        ),
+	.blend          ( blend            ),
+	.ypbpr          ( ypbpr            )
+	);
+
+// Samples
+
+wire [24:0] table_offset = core_mod == 3 ? 24'd29696 : 24'd26656;
+wire [24:0] wav_offset = table_offset + 8'd128;
+
+wire        wav_download = ioctl_downl && (ioctl_index == 0) && ioctl_addr >= wav_offset;
+reg  [24:0] wav_addr;
+wire [15:0] wav_data;
+reg         wav_want_byte;
+wire [15:0] samples_left;
+wire [15:0] samples_right;
+reg         use_samples;
+reg  [15:0] SoundTrigger;
+reg  [15:0] SoundStop;
+reg         Sound_Enable;
+
+// 8 bit write, 16 bit read
+
+sdram sdram (
+	.*,
+	.init(~pll_locked),
+	.clk(clk_vid),
+
+	.addr(ioctl_downl ? ioctl_addr-wav_offset : {wav_addr[24:1],1'd0}),
+	.we(wav_download && ioctl_wr),
+	.rd(~ioctl_downl & wav_want_byte),
+	.din(ioctl_dout),
+	.dout(wav_data),
+
+	.ready()
+);
+
+// Link to Samples module
+
+wire            samples_download = ioctl_downl && (ioctl_index == 0) && ioctl_addr >= table_offset && ioctl_addr < wav_offset;
+samples samples
+(
+	.audio_enabled(1'd1),
+	.audio_port_0(SoundTrigger[7:0]),
+	.audio_port_1(SoundTrigger[15:8]),
+	.audio_stop(SoundStop),
+
+	.wave_addr(wav_addr),
+	.wave_read(wav_want_byte),
+	.wave_data(wav_data),
+
+	.samples_ok(use_samples),
+
+	.dl_addr(ioctl_addr-table_offset),
+	.dl_wr(ioctl_wr),
+	.dl_data(ioctl_dout),
+	.dl_download(samples_download),
+
+	.CLK_SYS(clk_sys),
+	.clock(clk_vid),
+	.reset(reset),
+
+	.audio_in({2'b00, audio, 13'd0}),
+	.audio_out_L(samples_left),
+	.audio_out_R(samples_right)
+);
+
+dac #(
+	.C_bits(16))
+dac_l(
+	.clk_i(clk_sys),
+	.res_n_i(1),
+	.dac_i(samples_left),
+	.dac_o(AUDIO_L)
+	);
+
+dac #(
+	.C_bits(16))
+dac_r(
+	.clk_i(clk_sys),
+	.res_n_i(1),
+	.dac_i(samples_right),
+	.dac_o(AUDIO_R)
+	);
+
+
+// Common inputs
+wire m_up, m_down, m_left, m_right, m_fireA, m_fireB, m_fireC, m_fireD, m_fireE, m_fireF, m_upB, m_downB, m_leftB, m_rightB;
+wire m_up2, m_down2, m_left2, m_right2, m_fire2A, m_fire2B, m_fire2C, m_fire2D, m_fire2E, m_fire2F, m_up2B, m_down2B, m_left2B, m_right2B;
+wire m_up3, m_down3, m_left3, m_right3, m_fire3A, m_fire3B, m_fire3C, m_fire3D, m_fire3E, m_fire3F, m_up3B, m_down3B, m_left3B, m_right3B;
+wire m_up4, m_down4, m_left4, m_right4, m_fire4A, m_fire4B, m_fire4C, m_fire4D, m_fire4E, m_fire4F, m_up4B, m_down4B, m_left4B, m_right4B;
+wire m_tilt, m_coin1, m_coin2, m_coin3, m_coin4, m_one_player, m_two_players, m_three_players, m_four_players;
+
+arcade_inputs inputs (
+	.clk         ( clk_sys     ),
+	.key_strobe  ( key_strobe  ),
+	.key_pressed ( key_pressed ),
+	.key_code    ( key_code    ),
+	.joystick_0  ( joystick_0  ),
+	.joystick_1  ( joystick_1  ),
+	.rotate      ( rotate      ),
+	.orientation ( orientation ),
+	.joyswap     ( joyswap     ),
+	.oneplayer   ( 1'b0        ),
+	.controls    ( {m_tilt, m_coin4, m_coin3, m_coin2, m_coin1, m_four_players, m_three_players, m_two_players, m_one_player} ),
+	.player1     ( {m_upB, m_downB, m_leftB, m_rightB, 6'd0, m_fireF, m_fireE, m_fireD, m_fireC, m_fireB, m_fireA, m_up, m_down, m_left, m_right} ),
+	.player2     ( {m_up2B, m_down2B, m_left2B, m_right2B, 6'd0, m_fire2F, m_fire2E, m_fire2D, m_fire2C, m_fire2B, m_fire2A, m_up2, m_down2, m_left2, m_right2} ),
+	.player3     ( {m_up3B, m_down3B, m_left3B, m_right3B, 6'd0, m_fire3F, m_fire3E, m_fire3D, m_fire3C, m_fire3B, m_fire3A, m_up3, m_down3, m_left3, m_right3} ),
+	.player4     ( {m_up4B, m_down4B, m_left4B, m_right4B, 6'd0, m_fire4F, m_fire4E, m_fire4D, m_fire4C, m_fire4B, m_fire4A, m_up4, m_down4, m_left4, m_right4} )
+);
+
+endmodule 
---
+++ Arcade_MiST/Universal Cosmic Hardware/rtl/samples.vhd
@@ -0,0 +1,348 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity samples is
+port(
+	 -- Sound related
+	 audio_enabled  : in  std_logic;
+	 audio_port_0   : in  std_logic_vector( 7 downto 0);
+	 audio_port_1   : in  std_logic_vector( 7 downto 0);
+	 audio_stop     : in  std_logic_vector(15 downto 0);
+	 
+	 audio_in       : in  std_logic_vector(15 downto 0);
+	 audio_out_L    : out std_logic_vector(15 downto 0);
+	 audio_out_R    : out std_logic_vector(15 downto 0);
+	 
+	 -- Access to samples
+	 wave_addr      : out std_logic_vector(24 downto 0);
+	 wave_read      : out std_logic;
+	 wave_data      : in std_logic_vector(15 downto 0);
+	 
+	 -- table loading
+	 dl_addr        : in  std_logic_vector(24 downto 0);
+	 dl_wr          : in  std_logic;
+	 dl_data        : in  std_logic_vector( 7 downto 0);
+	 dl_download	 : in  std_logic;
+	 samples_ok     : out std_logic;
+	 
+	 -- Clocks and things
+	 CLK_SYS        : in  std_logic; -- (for loading table)
+	 clock          : in  std_logic; -- 43.264 Mhz (this drives the rest)  
+	 reset          : in  std_logic  -- high to reset
+ );
+end samples;
+
+architecture struct of samples is
+
+ -- Clock dividers
+ signal wav_clk_cnt  : std_logic_vector(11 downto 0); -- 44kHz divider / sound counter (43.264 Mhz count to 981 (x"3D5") for 44khz clock)
+ signal wav_freq_cnt : std_logic_vector(1 downto 0);  -- divide further to give 22Khz (0) and 11Khz (1)
+ signal wav_freq_lst : std_logic_vector(1 downto 0);  -- for rising edge checks
+ 
+ -- wave info (aka Table)
+ type addr_t is array (0 to 15) of std_logic_vector(23 downto 0);
+ type mode_t is array (0 to 15) of std_logic_vector(15 downto 0);
+  
+ signal wav_addr_start : addr_t;
+ signal wav_addr_end   : addr_t;
+ signal wav_mode       : mode_t := (others=>(others=>'0'));
+ signal table_loaded   : std_logic register := '0';
+ 
+ signal wave_left      : std_logic_vector(15 downto 0) register := (others=>'0'); 
+ signal wave_right     : std_logic_vector(15 downto 0) register := (others=>'0'); 
+ signal wave_read_ct   : std_logic_vector(2 downto 0) register := (others=>'0'); 
+ 
+ -- sound control info
+ signal snd_id : integer;
+ signal snd_addr_play  : addr_t := (others=>(others=>'1'));
+ signal ports          : std_logic_vector(15 downto 0); 
+ signal last_ports     : std_logic_vector(15 downto 0); 
+ signal this_ports     : std_logic_vector(15 downto 0); 
+ signal next_ports     : std_logic_vector(15 downto 0); 
+ signal this_stop      : std_logic_vector(15 downto 0); 
+ signal next_stop      : std_logic_vector(15 downto 0); 
+ 
+ -- Audio variables
+ signal audio_sum_l    : signed(19 downto 0);
+ signal audio_sum_r    : signed(19 downto 0);
+ signal audio_l        : signed(19 downto 0);
+ signal audio_r        : signed(19 downto 0);
+
+ begin
+
+----------------
+-- Table Load --
+----------------
+
+-- wav_mode - 8 bits - if byte = 00 then this bit does not trigger anything
+-- bit 0 = 11khz
+-- bit 1 = 22khz
+-- bit 2 = 44khz
+-- bit 4 = 16 bit (off = 8 bit)
+-- bit 5 = Stereo (off = mono)
+--
+-- trigger mode - 8 bits
+-- bit 0 = ON  one shot (sample plays once)
+-- bit 0 = OFF restarts if bit still active at end (loops)
+-- bit 1 = ON  cuts off sample if bit goes low (should it fade?)
+-- bit 1 = OFF continues until end of sample reached
+-- bit 4 = output LEFT channel
+-- bit 5 = output RIGHT channel (set both for MONO/STEREO)
+
+process (CLK_SYS,dl_download,dl_wr,dl_data)
+variable ID : integer;
+begin
+	if rising_edge(CLK_SYS) then
+	
+		if dl_download='1' and dl_wr='1' then
+		
+			ID := to_integer(unsigned(dl_addr(6 downto 3)));
+			
+			case dl_addr(2 downto 0) is
+				when "000" => -- Wave mode
+					wav_mode(ID)(7 downto 0) <= dl_data;
+					if dl_data(2 downto 0) /= "000" then
+						table_loaded <= '1';
+					end if;
+				when "001" => -- Trigger mode
+					wav_mode(ID)(15 downto 8) <= dl_data;
+				when "010" => -- Start Address
+					wav_addr_start(ID)(23 downto 16) <= dl_data;
+				when "011" => -- Start Address
+					wav_addr_start(ID)(15 downto 8) <= dl_data;
+				when "100" => -- Start Address
+					wav_addr_start(ID)(7 downto 0) <= dl_data;
+				when "101" => -- End Address
+					wav_addr_end(ID)(23 downto 16) <= dl_data;
+				when "110" => -- End Address
+					wav_addr_end(ID)(15 downto 8) <= dl_data;
+				when "111" => -- End Address
+					wav_addr_end(ID)(7 downto 0) <= dl_data;
+			end case;
+		end if;
+	end if;
+end process;
+ 
+-----------------
+-- Wave player --
+-----------------
+
+-- current IO bit & sample to be looking at
+snd_id <= to_integer(unsigned(wav_clk_cnt(11 downto 5)));
+ports  <= audio_port_1 & audio_port_0;
+samples_ok <= table_loaded;
+
+--wave_data <= wave_data1 & wave_data1;
+
+-- wave player
+process (clock, reset, table_loaded)
+begin
+	if table_loaded='1' then
+		if reset='1' then
+			wav_clk_cnt  <= (others=>'0');
+			wav_freq_cnt <= "00";
+			snd_addr_play <= (others=>(others=>'1'));
+			wave_read    <= '0';
+			audio_out_L <= x"0000";
+			audio_out_R <= x"0000";
+		else 
+			-- Use falling edge to interleave commands with SDRAM module
+			if falling_edge(clock) then
+			
+				-- make sure we don't miss any bits being set
+				next_ports <= next_ports or ports;
+				next_stop  <= next_stop or audio_stop;
+				
+				if snd_id <= 15 then
+					if snd_addr_play(snd_id)=x"FFFFFF" then
+						-- All Start play on 0 to 1 transition
+						if (last_ports(snd_id)='0' and this_ports(snd_id)='1') then
+							snd_addr_play(snd_id) <= wav_addr_start(snd_id);
+						end if;
+					else
+						-- cut out when signal zero
+						if (wav_mode(snd_id)(9)='1' and this_ports(snd_id)='0') then
+							snd_addr_play(snd_id) <= x"FFFFFF";
+						end if;
+						-- cut out when STOP set high
+						if (this_stop(snd_id)='1') then
+							-- But may just want to restart this sample
+							if this_ports(snd_id)='1' then
+								snd_addr_play(snd_id) <= wav_addr_start(snd_id);
+							else
+								snd_addr_play(snd_id) <= x"FFFFFF";
+							end if;
+						end if;
+					end if;
+				end if;
+				
+				-- 44.1kHz base tempo / high bits for scanning sound
+				if wav_clk_cnt = x"3D5" then  -- divide 43.264 Mhz by 981 => 44.102kHz
+				
+					wav_clk_cnt <= (others=>'0');
+					wav_freq_lst <= wav_freq_cnt;
+					wav_freq_cnt <= wav_freq_cnt + '1';
+
+					-- cycle along ports last / this
+					last_ports <= this_ports;
+					this_ports <= next_ports;
+					next_ports <= ports;
+
+					this_stop  <= next_stop;
+					next_stop  <= audio_stop;
+					
+					-- latch final audio / reset sum
+					audio_r <= audio_sum_r;
+					audio_l <= audio_sum_l;
+					audio_sum_r <= resize(signed(audio_in), 20);
+					audio_sum_l <= resize(signed(audio_in), 20);
+				else
+					wav_clk_cnt <= wav_clk_cnt + 1;
+				end if;
+				
+				if audio_enabled='1' then
+--					-- clip audio
+--					if  audio_r(19 downto 2) > 32767 then
+--						audio_out_R <= x"7FFF";
+--					elsif	audio_r(19 downto 2) < -32768 then 
+--						audio_out_R <= x"8000";
+--					else
+--						audio_out_R <= std_logic_vector(audio_r(17 downto 2));
+--					end if;
+--
+--					if  audio_l(19 downto 2) > 32767 then
+--						audio_out_L <= x"7FFF";
+--					elsif	audio_l(19 downto 2) < -32768 then 
+--						audio_out_L <= x"8000";
+--					else
+--						audio_out_L <= std_logic_vector(audio_l(17 downto 2));
+--					end if;
+
+					audio_out_R <= std_logic_vector(audio_r(17 downto 2));
+					audio_out_L <= std_logic_vector(audio_l(17 downto 2));
+
+				else
+					audio_out_L <= x"0000";
+					audio_out_R <= x"0000";
+				end if;
+
+				-- sdram read trigger (and auto refresh period)
+				if wav_clk_cnt(4 downto 0) = "00001" then wave_read <= '1';end if;
+				if wav_clk_cnt(4 downto 0) = "00011" then wave_read <= '0';end if;
+				
+				-- select only useful cycles (0-15)
+				if snd_id <= 15 then 
+				
+					-- is this sample present
+					if wav_mode(snd_id)(2 downto 0) /= "000" then
+				
+						if snd_addr_play(snd_id) /= x"FFFFFF" then
+		
+							---------------
+							-- Data read --
+							---------------
+							
+							-- set addr for first byte (but it reads 4 bytes anyway)
+							if wav_clk_cnt(4 downto 0) = "00000" then
+								wave_addr <= '0' & snd_addr_play(snd_id);
+							end if;
+						
+							if wav_clk_cnt(4 downto 0) = "01000" then -- "11101" then
+									-- SDRAM bit : data returned, put into left / right accordingly
+									case wav_mode(snd_id)(5 downto 4) is
+									
+										when "00" => -- 8 bit mono
+											if snd_addr_play(snd_id)(0)='0' then
+												-- Low byte
+												wave_left <= (not wave_data(7)) & wave_data(6 downto 0) & x"00";
+												wave_right <= (not wave_data(7)) & wave_data(6 downto 0) & x"00";
+											else
+												-- high byte
+												wave_left <= (not wave_data(15)) & wave_data(14 downto 8) & x"00";
+												wave_right <= (not wave_data(15)) & wave_data(14 downto 8) & x"00";
+											end if;
+											
+										when "01" => -- 16 bit mono
+											wave_left <= wave_data;											
+											wave_right <= wave_data;											
+											
+										when "10" => -- 8 bit stereo
+											wave_left <= (not wave_data(7)) & wave_data(6 downto 0) & x"00";
+											wave_right <= (not wave_data(15)) & wave_data(14 downto 8) & x"00";
+											
+										when "11" => -- 16 bit stereo (won't work with curent SDRAM controller!)
+											wave_left <= wave_data;											
+											wave_right <= wave_data;											
+											
+									end case;
+							end if;
+							
+							-- Data all read, add to output counters
+							if wav_clk_cnt(4 downto 0) = "01001" then -- "111110" then
+							
+								-- Left channel
+								if wav_mode(snd_id)(12)='1' then
+									audio_sum_l <= audio_sum_l + to_integer(signed(wave_left));
+								end if;
+								
+								-- Right channel
+								if wav_mode(snd_id)(13)='1' then
+									audio_sum_r <= audio_sum_r + to_integer(signed(wave_right));
+									--audio_sum_r <= audio_sum_r + to_integer(signed(samp_data));
+								end if;
+						
+								--wave_left  <= x"0000";
+								--wave_right <= x"0000";
+
+								-- Increment address depending on frequency and size
+								if wav_mode(snd_id)(2)='1' or 
+								  (wav_mode(snd_id)(1)='1' and wav_freq_lst(0)='0' and wav_freq_cnt(0)='1') or
+								  (wav_mode(snd_id)(0)='1' and wav_freq_lst(1)='0' and wav_freq_cnt(1)='1') then
+								  
+								  case wav_mode(snd_id)(5 downto 4) is
+										when "00" => 
+											-- 8 bit mono
+											snd_addr_play(snd_id) <= snd_addr_play(snd_id) + 1;
+										when "01" | "10" =>
+											-- 16 bit mono or 8 bit stereo
+											snd_addr_play(snd_id) <= snd_addr_play(snd_id) + 2;
+										when "11" =>
+											-- 16 bit stereo 
+											snd_addr_play(snd_id) <= snd_addr_play(snd_id) + 4;
+								  end case;
+
+								end if;
+															
+							end if;
+							
+							if wav_clk_cnt(4 downto 0) = "01111" then -- "111111" then
+								-- End of Wave data ?
+								if snd_addr_play(snd_id) > wav_addr_end(snd_id) then 	
+									-- Restart ?
+									if (wav_mode(snd_id)(8)='0' and this_ports(snd_id)='1') then
+										-- Loop back to the start
+										snd_addr_play(snd_id) <= wav_addr_start(snd_id);
+									else
+										-- Stop
+										snd_addr_play(snd_id) <= x"FFFFFF";
+									end if;									
+								end if;
+							end if;
+							
+						end if; -- Playing
+
+					end if; -- Bit Active
+					
+				end if; -- useful
+				
+			end if; -- rising clock
+
+		end if; -- reset
+		
+	end if; -- table loaded
+
+end process;
+
+end;
