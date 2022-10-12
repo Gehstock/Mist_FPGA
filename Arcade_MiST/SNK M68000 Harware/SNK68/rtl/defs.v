4b4f4c957edbcfbc815dee730274a6d946b27fd2
Gyorgy Szombathelyi <gyurco@freemail.hu>
2022-10-13 00:44:54 +0200

    SNK68

---
+++ Arcade_MiST/SNK M68000 Harware/SNK68/rtl/defs.v
@@ -0,0 +1,3 @@
+localparam pcb_A7007_A8007 = 0;  // [ikari3], [searchar], [streetsmj, streetsm1, streetsmw] - Ikari III, S.A.R., and Street Smart V1 (mame nomenclature, would be V2)
+localparam pcb_A7008       = 1;  // [pow] - P.O.W.
+localparam pcb_A7008_SS    = 2;  // [streetsm] - Street Smart V2 (mame nomenclature, would be V1)
